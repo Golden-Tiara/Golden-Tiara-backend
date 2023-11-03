83b539531dc1957231556ca536da397dcdd7f748
pingphawat <phawat.po@ku.th>
2023-11-03 21:39:33 +0630

    update fix api.php

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,16 @@ use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// ... ส่วนอื่น ๆ ...
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -22,15 +31,21 @@ Route::group([
 
 ], function ($router) {
 
-    // ... ส่วนที่เกี่ยวกับ authentication ...
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::get('user', [UserController::class, 'show']);
+    Route::post('user/update', [UserController::class, 'update']);
 
 });
 
-// เพิ่ม middleware ที่คุณต้องการเพื่อให้ตรวจสอบสิทธิ์
+
 Route::apiResource('/pawn', PawnController::class);
 Route::apiResource('/examination', ExaminationController::class);
 Route::apiResource('/gold', GoldController::class);
+// Route::apiResource('/pawn', PawnController::class);
 Route::apiResource('/transaction', TransactionController::class);
 
 Route::get('/user/check/{nationalId}', [UserController::class, 'findUserByNationalId']);
-
